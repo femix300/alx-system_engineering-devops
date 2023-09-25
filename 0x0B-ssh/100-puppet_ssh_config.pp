4053965a5870c2afdf26dceacd9932f6f0ddee1d
Peter Ajimoti <oluwaseyipeter450@gmail.com>
2023-09-25 05:45:45 +0100

    added task 100 (advance task 1)

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,10 @@
+# A puppet manifest to make changes to an ssh configuration file
+file { '/home/peter_ajimoti/.ssh/config':
+  ensure  => present,
+  content => 'Host *
+    HostName 3.83.18.144
+    User ubuntu
+    PasswordAuthentication no
+    IdentityFile ~/.ssh/school
+  ',
+}
