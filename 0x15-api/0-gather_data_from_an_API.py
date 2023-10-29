3518ed8202e964d0d9bbb9fec4d39a90e379655e
Peter Ajimoti <oluwaseyipeter450@gmail.com>
2023-10-29 01:17:21 +0100

    modified task 0

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,14 +1,27 @@
 #!/usr/bin/python3
-"""Returns to-do list information for a given employee ID."""
+"""Returns information about an employees to do list"""
 import requests
 import sys
 
 if __name__ == "__main__":
-    url = "https://jsonplaceholder.typicode.com/"
-    user = requests.get(url + "users/{}".format(sys.argv[1])).json()
-    todos = requests.get(url + "todos", params={"userId": sys.argv[1]}).json()
+    employee_id = sys.argv[1]
+    baseurl = 'https://jsonplaceholder.typicode.com/users/'
+    url = baseurl + employee_id
+    todourl = baseurl + employee_id + '/todos'
+    r = requests.get(url)
+    todos = requests.get(todourl).json()
+    data = r.json()
 
-    completed = [t.get("title") for t in todos if t.get("completed") is True]
-    print("Employee {} is done with tasks({}/{}):".format(
-        user.get("name"), len(completed), len(todos)))
-    [print("\t {}".format(c)) for c in completed]
+    employeename = data['name']
+    done_tasks = 0
+    tasks = 0
+
+    for item in todos:
+        if item['completed']:
+            done_tasks += 1
+        tasks += 1
+
+    print(f"Employee {employeename} is done with tasks({done_tasks}/{tasks}):")
+    for title in todos:
+        if title['completed']:
+            print(f"\t {title['title']}")
